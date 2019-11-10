7bd86855f5cab89c063637e63f60046176f88b10
Melissa Chiang <mchiang62@gmail.com>
2019-11-10 09:04:56 -0500

    json/sql files

---
+++ bamazon.sql
@@ -0,0 +1,39 @@
+-- console.log
+-- closingg curly braces and brackets
+
+DROP DATABASE IF EXISTS bamazonDB;
+
+CREATE DATABASE bamazonDB;
+
+USE bamazonDB;
+
+CREATE TABLE products (
+    id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(100) NOT NULL,
+
+
+
+--   id INT NOT NULL AUTO_INCREMENT,
+--   flavor VARCHAR(45) NULL,
+--   price DECIMAL(10,2) NULL,
+--   quantity INT NULL,
+--   PRIMARY KEY (id)
+);
+
+INSERT INTO products ()
+VALUES ();
+
+INSERT INTO products ()
+VALUES ();
+
+INSERT INTO products ()
+VALUES ();
+
+-- INSERT INTO products (flavor, price, quantity)
+-- VALUES ("vanilla", 2.50, 100);
+
+-- INSERT INTO products (flavor, price, quantity)
+-- VALUES ("chocolate", 3.10, 120);
+
+-- INSERT INTO products (flavor, price, quantity)
+-- VALUES ("strawberry", 3.25, 75);
